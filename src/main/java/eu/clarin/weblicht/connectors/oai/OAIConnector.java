d937cd3fe7168c4cadbd4005cc2e23147f6131a5
Alexander Kislev <alexander.kislev@uni-tuebingen.de>
2013-02-28 18:33:34 +0000

    preliminary support of resumptionToken

--- src/main/java/eu/clarin/weblicht/connectors/oai/OAIConnector.java
+++ src/main/java/eu/clarin/weblicht/connectors/oai/OAIConnector.java
@@ -11,9 +11,11 @@ import eu.clarin.weblicht.bindings.cmd.cp.SimpleMetadataScheme;
 import eu.clarin.weblicht.bindings.cmd.cp.SimpleWebServiceType;
 import eu.clarin.weblicht.bindings.oai.OAIPMH;
 import eu.clarin.weblicht.bindings.oai.Record;
+import eu.clarin.weblicht.bindings.oai.ResumptionToken;
 import eu.clarin.weblicht.connectors.AbstractConnector;
 import eu.clarin.weblicht.connectors.ConnectorException;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
@@ -28,6 +30,7 @@ public class OAIConnector extends AbstractConnector {
     private static final String METADATA_PREFIX = "metadataPrefix";
     private static final String SET = "set";
     private static final String LIST_RECORDS = "ListRecords";
+    private static final String RESUMPTION_TOKEN = "resumptionToken";
     private static final String CMDI = "cmdi";
 
     public OAIConnector(Integer timeoutMilliseconds) {
@@ -40,17 +43,42 @@ public class OAIConnector extends AbstractConnector {
 
     public List<Record> retrieveRecords(URI uri) throws ConnectorException {
         WebResource oaiResource = buildWebResource(uri, null);
-        return retrieveOAIPMH(oaiResource).getRecords();
+        return retrieveRecords(oaiResource);
     }
 
     public List<Record> retrieveRecords(URI uri, String webServiceSet) throws ConnectorException {
         WebResource oaiResource = buildWebResource(uri, webServiceSet);
-        return retrieveOAIPMH(oaiResource).getRecords();
+        return retrieveRecords(oaiResource);
     }
 
-    public OAIPMH retrieveOAIPMH(Metadata metadata) throws ConnectorException {
+    public List<Record> retrieveRecords(Metadata metadata) throws ConnectorException {
         WebResource oaiResource = buildWebResource(metadata);
-        return retrieveOAIPMH(oaiResource);
+        return retrieveRecords(oaiResource);
+    }
+
+    public List<Record> retrieveRecords(WebResource oaiResource) throws ConnectorException {
+        List<Record> records = null;
+        WebResource nextOAIResource = oaiResource;
+        while (true) {
+            OAIPMH oaipmh = retrieveOAIPMH(nextOAIResource);
+            if (oaipmh.getListRecords() == null || oaipmh.getListRecords().getRecords() == null) {
+                throw new ConnectorException(oaipmh.getError() != null ? oaipmh.getError() : "oai-pmh response contains no records");
+            }
+            ResumptionToken resumptionToken = oaipmh.getListRecords().getResumptionToken();
+            if (resumptionToken != null) {
+                if (records == null) {
+                    records = new ArrayList<Record>();
+                }
+                records.addAll(oaipmh.getListRecords().getRecords());
+                if (resumptionToken.getValue() != null && !resumptionToken.getValue().isEmpty()) {
+                    nextOAIResource = oaiResource.queryParam(RESUMPTION_TOKEN, resumptionToken.getValue());
+                } else {
+                    return records;
+                }
+            } else {
+                return oaipmh.getListRecords().getRecords();
+            }
+        }
     }
 
     public OAIPMH retrieveOAIPMH(WebResource oaiResource) throws ConnectorException {
@@ -63,14 +91,16 @@ public class OAIConnector extends AbstractConnector {
         }
     }
 
-    public WebResource buildWebResource(Metadata metadata) {
+    public WebResource buildWebResource(Metadata metadata) throws ConnectorException {
         URI uri = metadata.getOaiAccessPoint();
         String webServiceSet = metadata.getWebServicesSet();
+        if (webServiceSet == null || webServiceSet.isEmpty()) {
+            throw new ConnectorException("WebServicesSet is undefined");
+        }
         if (uri != null && uri.getQuery() != null) {
             return buildWebResource(uri, webServiceSet);
-
         }
-        return null;
+        throw new ConnectorException("bad oai access point uri: " + uri);
     }
 
     public WebResource buildWebResource(URI uri, String webServiceSet) {
